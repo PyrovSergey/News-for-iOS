82b11d27715f76cb180b0a5c671c531f41dd8ec9
serge <pyrov.sergey@gmail.com>
2019-02-18 15:02:11 +0300

    Added fully working page loading indicator

--- TestTabBarApp/Controllers/ArticleViewController.swift
+++ TestTabBarApp/Controllers/ArticleViewController.swift
@@ -12,39 +12,31 @@ import WebKit
 class ArticleViewController: UIViewController, WKNavigationDelegate {
     
     var articleUrl: String?
-    var webView: WKWebView!
-    
-    override func loadView() {
-        webView = WKWebView()
-        webView.navigationDelegate = self
-        view = webView
-    }
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.isHidden = true
+        progressView!.translatesAutoresizingMaskIntoConstraints = false
+        progressView.setProgress(0.0, animated: true)
         self.tabBarController?.tabBar.isHidden = true
         if let url = URL(string: articleUrl!) {
             webView.load(URLRequest(url: url))
-            //webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+            webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
             webView.allowsBackForwardNavigationGestures = true
         }
-        
-        
-//        progressView = UIProgressView(progressViewStyle: .bar)
-//        progressView.center = self.view.center
-//        //progressView.frame = CGRectMake(0,0,50,20)
-//        progressView.translatesAutoresizingMaskIntoConstraints = false
-//        progressView.setProgress(0.5, animated: false)
-//        self.view.addSubview(progressView)
-
-        // Do any additional setup after loading the view.
     }
     
-//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//        if keyPath == "estimatedProgress" {
-//            print(Float(newsWebView.estimatedProgress))
-//            progressView.progress = Float(newsWebView.estimatedProgress)
-//        }
-//    }
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if Float(webView.estimatedProgress) == 1.0 {
+            webView.isHidden = false
+            progressView.isHidden = true
+        } 
+        if keyPath == "estimatedProgress" {
+            print(Float(webView.estimatedProgress))
+            progressView!.progress = Float(webView.estimatedProgress)
+        }
+    }
 
 }
